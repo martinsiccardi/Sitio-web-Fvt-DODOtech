ef59793c8c625d584d3e836d90bdc05d85cd0b0c
IgnacioGabrielRodriguez <igrodriguez@escuelasproa.edu.ar>
2024-06-04 14:42:41 -0300

    index

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function() {
     var dropdownBtn = document.querySelector('.dropdown-btn');
     var dropdownContent = document.querySelector('.dropdown-content');
     
